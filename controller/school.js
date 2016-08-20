6356c2516ec3ef353eee649a9b5cfbd3d274941b
abdooageel <ageel.abdoo@gmail.com>
2016-08-20 12:08:38 +0200

    scool info

--- controller/school.js
+++ controller/school.js
@@ -1,18 +1,27 @@
 
 var model = require("../models");
+var jsonfile = require('jsonfile');
+var file = './school.json' 
 
 module.exports = {
-  getSchoolInfo :function(cb){
-    var info = {
-      name:"مدرسة التوفيق للتعليم الحر",
-      address:"حي الاندلس شارع البريد بالقرب من المستوصف الصحي",
-      phone:"0928935974",
-      des:"مدرسة التوفيق للتعليم الحر في مختلف المستويات نطمح دائما لنكون الافضل كن مطمئنا بوجود ابنائك برفقة طاقم مدرسة التوفيق"
-    };
-    cb(info);
+  getSchoolInfo :function(cb){  
+  jsonfile.readFile(file, function(err, obj) {
+    if(obj){
+      cb(obj);
+    }else{
+      cb(null);
+    }
+    
+  });
   },
 
   updateSchool : function(body,cb){
-    cb(true);
-  },
+    jsonfile.writeFile(file, body, function (err) {
+      if(!err){
+        cb(true);
+      }else{
+        cb(false);
+      }
+    });
+  }
 };
