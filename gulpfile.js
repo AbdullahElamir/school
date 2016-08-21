879eba6f0b8407ff1ce1d9469abd7b33f014af66
Aladdin Sonni <alaaden3000@gmail.com>
2016-08-21 21:10:22 +0200

    remove unused function in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,17 +11,7 @@ var paths = {
   html: ['./views/**/*.html'],
 };
 
-gulp.task('default',['html','start','watch']);
-
-gulp.task('start',function(){
-  req.nodemon({
-    script: './bin/www',
-    ext: 'html js',
-    watch: ['bin/*','routes/*','app.js']
-  }).on('restart',function(){
-    console.log('The server has been restarted!');
-  });
-});
+gulp.task('default',['html']);
 
 gulp.task('html',function(){
   gulp.src(paths.html)
@@ -30,8 +20,4 @@ gulp.task('html',function(){
   .pipe(req.htmlhint())
   .pipe(req.htmlhint.reporter("htmlhint-stylish"))
   .pipe(gulp.dest('./views/'));
-});
-
-gulp.task('watch',function(){
-  gulp.watch(paths.html,['html']);
 });
