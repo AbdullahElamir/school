2017815d7c851913fdc727ff60acf7810c63f044
Abdorihan <abdorihan@yahoo.com>
2016-09-22 11:43:39 +0200

    teacherSide

--- public/js/teachers/services.js
+++ public/js/teachers/services.js
@@ -8,8 +8,8 @@
       }
     };
     return self;
-  }]); 
-  
+  }]);
+
   app.service('studentInformationServ',['$http',function($http){
     var self = {
       'getStudentsInfo': function(){
@@ -17,30 +17,12 @@
       }
     };
     return self;
-  }]); 
+  }]);
 
-  app.service('ClassServ',['$http',function($http){
+  app.service('ClassRoomServ',['$http',function($http){
     var self = {
-      'addClass': function(_class){
-        return $http.post('/class/add',_class);
-      },
-      'getClasses': function(limit,page){
-        return $http.get('/class/'+limit+'/'+page);
-      },
-      'getClassesBySearchValue': function(searchValue,limit,page){
-        return $http.get('/class/'+searchValue+'/'+limit+'/'+page);
-      },
-      'deleteClass': function(id){
-        return $http.delete('/class/delete/'+id);
-      },
-      'getClassById': function(id){
-        return $http.get('/class/'+id.id);
-      },
-      'editClass': function(id,obj){
-        return $http.put('/class/edit/'+id,obj);
-      },
-      'getAllClasses': function(){
-        return $http.get('/class/all');
+      'getTeacherClassRooms': function(id){
+        return $http.get('/classRoom/teacher/'+id);
       }
     };
     return self;
