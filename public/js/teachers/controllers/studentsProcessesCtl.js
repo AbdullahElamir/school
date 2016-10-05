799533ec4954f915fe86d12294d7f2ddfd13d30e
TahaAlmahmudi <tahaomar2731994@facebook.com>
2016-10-05 10:41:55 +0200

    fix some text

--- public/js/teachers/controllers/studentsProcessesCtl.js
+++ public/js/teachers/controllers/studentsProcessesCtl.js
@@ -4,7 +4,7 @@
 
   app.controller('StudentsProcessesCtl',['$scope','studentsProcessesServ','ClassRoomServ',function($scope,studentsProcessesServ,ClassRoomServ){
     // id of teacher
-    var id ='57c15717e49f256f03b16064';
+    var id ='57df0e437fb8ad40ec8b48c2';
     ClassRoomServ.getTeacherClassRooms(id).then(function(response){
       $scope.classRooms = response.data;
     },function(response){
