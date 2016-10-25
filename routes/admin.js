93fe2b6404713552cfeeabebc8ae76b56b2086bb
abdullah <abdullah.el_ameer@yahoo.com>
2016-10-25 11:06:42 +0200

    active eslintrc

--- routes/admin.js
+++ routes/admin.js
@@ -10,6 +10,10 @@ router.get('/:folder/:student/:name', function(req, res) {
   var name = req.params.name;
   res.render('admin/' + folder +'/'+ student + '/' + name);
 });
+
+
+
+
 router.get('/:folder/:name', function(req, res) {
   var folder = req.params.folder;
   var name = req.params.name;
