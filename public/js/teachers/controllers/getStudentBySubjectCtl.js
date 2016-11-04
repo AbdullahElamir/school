4c25ce38cb8a0dfafefa591909072679d9da6017
MohamedAlgmate <mohamed199188@gmail.com>
2016-11-04 22:58:08 +0200

    fix

--- public/js/teachers/controllers/getStudentBySubjectCtl.js
+++ public/js/teachers/controllers/getStudentBySubjectCtl.js
@@ -3,9 +3,7 @@
   var app = angular.module('teachersSchool');
 
   app.controller('getStudentBySubjectCtl',['$scope','getStudentBySubjectServ','toastr',function($scope,getStudentBySubjectServ,toastr){
-    // id of teacher
-
-    // var id ='57fcc3ac1f20ef6c8f545e33';
+    
     $scope.getStudentBySubjectForm ={};
 
   }]);
